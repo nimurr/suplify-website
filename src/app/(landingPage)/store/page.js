dc1fb34bf10664c357d6f9f19f922d1cba757c14
nimurr <nimurnerob404@gmail.com>
2025-11-03 11:20:45 +0600

    `Remove md:pt-40 class from div element in page.js`

--- src/app/(landingPage)/store/page.js
+++ src/app/(landingPage)/store/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='py-16 pt-28 md:pt-40 flex justify-center items-center'>
+        <div className='py-16 pt-28 flex justify-center items-center'>
             <TabPage />
         </div>
     );
