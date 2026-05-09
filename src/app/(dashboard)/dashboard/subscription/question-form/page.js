e18dbc740663b1c49221801e09c908570699c518
nimurr <nimurnerob404@gmail.com>
2026-05-09 09:48:02 +0600

    `Update navigate.push URL in question-form page`

--- src/app/(dashboard)/dashboard/subscription/question-form/page.js
+++ src/app/(dashboard)/dashboard/subscription/question-form/page.js
@@ -44,7 +44,7 @@ const Page = () => {
 
             if (response?.code === 200 || response?.code === 201) {
                 toast.success(response?.message || 'Answers submitted successfully!');
-                navigate.push(`${response?.data?.attributes}`);
+                navigate.push(`/dashboard/subscription`);
                 form.resetFields();
             } else {
                 toast.error(response?.message || 'Failed to submit answers');
